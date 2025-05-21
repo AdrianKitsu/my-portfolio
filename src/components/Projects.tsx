01bd06ecfc7878832ef4ecf082c8f58f25069318
Adrian <aborges@trewknowledge.com>
2025-05-20 23:27:03 -0400

    styling update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,43 +6,56 @@ import Tempehfy from "../assets/images/tempehfy.png";
 import Mitchells from "../assets/images/mitchells.png";
 import GlossHover from "./GlossHover";
 
+const projectData = [
+  {
+    image: FutureLaunch,
+    alt: "Future Launch",
+    portfolioRedirect: "portfolio/#tab-fl",
+    clientName: "RBC - Future Launch",
+  },
+  {
+    image: Mitchells,
+    alt: "Mitchells",
+    portfolioRedirect: "portfolio/#tab-mitch",
+    clientName: "Mitchell's Food",
+  },
+  {
+    image: Tempehfy,
+    alt: "Tempehfy",
+    portfolioRedirect: "portfolio/#tab-tempeh",
+    clientName: "Lightlife",
+  },
+  {
+    image: Gns,
+    alt: "Grab n Snack",
+    portfolioRedirect: "portfolio/#tab-gns",
+    clientName: "Grab 'N Snack",
+  },
+  {
+    image: ThoughtLeadership,
+    alt: "Thought Leadership",
+    portfolioRedirect: "portfolio/#tab-tl",
+    clientName: "RBC - Thought Leadership",
+  },
+];
+
 const Projects = () => {
   return (
     <>
       <h2 className="mb-[10px] text-[40px] sm:text-[60px] text-center text-[#fcfcfc] sm:tracking-[4px] sm:text-2xl">
         Projects I've Worked On
       </h2>
-      <div className="panel-container grid sm:grid-cols-1 md:grid-cols-3 gap-5 sm:gap-10 justify-items-center sm:mx-auto sm:max-w-fit ">
-        <GlossHover
-          image={FutureLaunch}
-          alt="Future Launch"
-          href="https://www.rbc.com/en/future-launch/"
-          portfolioRedirect="portfolio#tab-fl"
-        />
-        <GlossHover
-          image={Gns}
-          alt="Grab n Snack"
-          href="https://www.grabnsnack.ca/"
-          portfolioRedirect="portfolio#tab-gns"
-        />
-        <GlossHover
-          image={Tempehfy}
-          alt="Tempehfy"
-          href="https://lightlife.com/tempehfy/"
-          portfolioRedirect="portfolio#tab-tempeh"
-        />
-        <GlossHover
-          image={ThoughtLeadership}
-          alt="Thought Leadership"
-          href="https://www.rbc.com/en/thought-leadership/"
-          portfolioRedirect="portfolio#tab-tl"
-        />
-        <GlossHover
-          image={Mitchells}
-          alt="Mitchells"
-          href="https://mitchellsfoods.ca/"
-          portfolioRedirect="portfolio#tab-mitch"
-        />
+      <div className="panel-container grid sm:grid-cols-1 md:grid-cols-3 gap-5 sm:gap-x-10 sm:gap-y-1 justify-items-center sm:mx-auto sm:max-w-fit">
+        {projectData.map((project, index) => (
+          <GlossHover
+            key={index}
+            image={project.image}
+            alt={project.alt}
+            portfolioRedirect={project.portfolioRedirect}
+            clientName={project.clientName}
+            index={index}
+          />
+        ))}
       </div>
     </>
   );
