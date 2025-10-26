6c10bc4032010e8d60dc319f6308aa113f439da4
Adrian <aborges@trewknowledge.com>
2025-10-26 19:26:51 -0400

    analytics and speed insights added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <AppRoutes />
+      <Analytics />
+      <SpeedInsights />
     </Router>
   );
 };
